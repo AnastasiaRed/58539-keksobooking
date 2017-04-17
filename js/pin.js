512b2e0f92bdb71a4aa73f2bc32d714ba904014a
AnastasiaRed <sta.sha@mail.ru>
2017-04-17 22:51:48 +0300

    make main pin draggable

--- js/pin.js
+++ js/pin.js
@@ -21,12 +21,70 @@ window.pinUtils = (function () {
     window.cardUtils.showCard();
   };
 
+  var mapWrap = document.querySelector('.tokyo');
+  var mapWrapHeight = mapWrap.offsetHeight;
+  var mapWrapWidth = mapWrap.offsetWidth;
+
+  var PIN_MAIN_WIDTH = 75;
+  var PIN_MAIN_HEIGHT = 94;
+  var pinMain = document.querySelector('.pin__main');
+  pinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var pinShiftTop = pinMain.offsetTop - shift.y;
+      var pinShiftLeft = pinMain.offsetLeft - shift.x;
+
+      if ((pinShiftTop < 0) || ((pinShiftTop + PIN_MAIN_HEIGHT) > mapWrapHeight) || (pinShiftLeft < 0) || ((pinShiftLeft + PIN_MAIN_WIDTH) > mapWrapWidth)) {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      }
+      else{
+        pinMain.style.top = pinShiftTop + 'px';
+        pinMain.style.left = pinShiftLeft + 'px';
+
+        var adressX = (pinMain.offsetTop - shift.y) - PIN_MAIN_WIDTH;
+        var adressY = (pinMain.offsetLeft - shift.x) - PIN_MAIN_HEIGHT/2;
+        var adressInput = document.querySelector('#address');
+        adressInput.value = 'x: ' + adressX + ', y: ' + adressY;
+      }
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+
+
   return {
     generatePins: function (offers) {
 
       var PIN_WIDTH = 40;
       var PIN_HEIGHT = 40;
-
       var fragment = document.createDocumentFragment();
 
       for (var i = 0; i < offers.length; i++) {
